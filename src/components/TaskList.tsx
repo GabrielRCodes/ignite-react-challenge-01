68fc663ca01b9c27dce446f661618e049d60ed54
Gabriel Rodrigues <76919248+GabrielRCodes@users.noreply.github.com>
2021-12-13 19:28:57 -0300

    Update TaskList.tsx

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -51,7 +51,7 @@ export function TaskList() {
 
         <div className="input-group">
           <input 
-            type="text 
+            type="text" 
             placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
